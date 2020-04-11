2b32ff0673cb708f4ecd7fd439361e596abf3f46
MikeC <cruz.michael.j@gmail.com>
2020-04-11 16:18:48 -0400

    added csv download functionality

--- API/app2-aws.py
+++ API/app2-aws.py
@@ -1,7 +1,7 @@
 import psycopg2
 from flask import Flask, jsonify
 from flask_cors import CORS
-
+from flask import send_file
 
 app = Flask(__name__)
 CORS(app)
@@ -15,14 +15,16 @@ cur = conn.cursor()
 def welcome():
     """List all available api routes."""
     return (
-        f"<h2>Welcome to Market Data API </h2><br/>"
+        f"<h1>Welcome to Market Data API </h1><br/>"
+        f"<h3>Returns Time Series data for various Stock Market Indexes</h3>"
+        f"<h3>Includes :  Date, Open, High, Low, Close, Volume, Ticker and Daily Change</h3>"
         f"Available Routes:<br/>"
-        f"/api/v1.0/index/market<br/>"
-        f"n=DJI,FTSE,GSPC,N225,HSI"
+        f"<b>/market</b> Get data for Dow Jones Industrial, S&P 500, FTSE100, Nikkei 225 and Hang Seng Index<br/>"
+        f"<b>/getCSV</b> get full dataset in CSV format<br/>"
         f"<br>   </br>"        
     )
 
-@app.route("/api/v1.0/index/market")
+@app.route("/market")
 def market():
     try:
         cur.execute('SELECT * FROM index_table')
@@ -37,6 +39,12 @@ def market():
         print("I am here")
         return (f"<h2>An error occured</h2>")     
         
+@app.route('/getCSV')
+def plot_csv():
+    return send_file('out.csv',
+                     mimetype='text/csv',
+                     attachment_filename='out.csv',
+                     as_attachment=True)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
